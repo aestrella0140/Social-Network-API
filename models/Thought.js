e5a751ce89ad4f9edc22211760507e5b41bc734d
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2023-10-24 18:33:47 -0700

    fixed user model into right folder and new thought model

---
+++ models/Thought.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require('mongoose');
+const moment = require('moment');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            Type: String,
+            Required: true,
+            minlenght: 1,
+            maxlength: 280,
+        },
+        createdAt: {
+            Type: Date,
+            default: Date.now,
+            get: (timeStamp) => moment(timeStamp).format('MMMM Do YYYY, h:mm:ss a'),
+        },
+        username: {
+            Type: String,
+            Required: true,
+        },
+        reactions: [reactionSchema],
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
